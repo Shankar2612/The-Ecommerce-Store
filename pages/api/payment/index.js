ab0e89a778bcb0d9083ac863a3b39b7738458db3
unknown <bapu.2612@gmail.com>
2021-10-07 19:21:20 +0530

    removing major flaws

--- pages/api/payment/index.js
+++ pages/api/payment/index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import Razorpay from "razorpay";
 import uniqid from 'uniqid';
+require("dotenv").config();
 
 const app = express();
 
 export default app.post("/api/payment", (req, res) => {
     console.log((req.body.amount*100).toFixed(0));
     let instance = new Razorpay({
-        key_id: "rzp_test_AXPxBUPNlQkmNe",
-        key_secret: 'Z83jiSEWPcVZQQmac6xQF0G7',
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_KEY_SECRET,
     });
 
     const options = {
